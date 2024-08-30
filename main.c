336de2ad394763d9012450919203546b5348404e
YgoRosa <ygor18rosa@gmail.com>
2024-08-30 16:26:54 -0300

    Correção e Questão 2

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // https://github.com/YgoRosa/CalculadoraBinaria
 // Ygor Rosa - 3ºA 
 // Implementação da função de conversão de decimal para binário, octal, hexadecimal e BCD às 22:23 do dia 29/08/2024
-//
+// Correção da função Octal e Implementação da função complementado A2, 30/08/24 às 16:25
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -70,23 +70,56 @@ void decimalParaHexadecimal(int numero) {
 }
 
 void decimalParaBCD(int numero) {
-    int bcd[32];
+    int bcd[16]; 
     int i = 0;
 
     printf("Conversao para BCD:\n");
+
     while (numero > 0) {
-        bcd[i] = numero % 10;
-        printf("Dígito: %d\n", bcd[i]);
+        int digito = numero % 10;
+        printf("Digito: %d\n", digito);
+
+        bcd[i++] = digito & 1;
+        bcd[i++] = (digito & 2) >> 1;
+        bcd[i++] = (digito & 4) >> 2;
+        bcd[i++] = (digito & 8) >> 3;
+        
         numero = numero / 10;
-        i++;
     }
 
     printf("Numero em BCD: ");
     for (int j = i - 1; j >= 0; j--) {
-        printf("%04d ", bcd[j]);
+        printf("%d", bcd[j]);
+        if (j % 4 == 0) {
+            printf(" "); 
+        }
     }
     printf("\n\n");
 }
+
+void decimalParaComplemento2(int numero) {
+    unsigned short complemento2[16];
+    int i = 15;
+
+    printf("Conversao para complemento a 2 com 16 bits:\n");
+
+    if (numero < 0) {
+        numero = (1 << 16) + numero; 
+    }
+
+    while (i >= 0) {
+        complemento2[i] = numero % 2;
+        numero = numero / 2;
+        i--;
+    }
+
+    printf("Numero em complemento a 2 com 16 bits: ");
+    for (int j = 0; j < 16; j++) {
+        printf("%d", complemento2[j]);
+    }
+    printf("\n\n");
+}
+
 
 int main() {
     int numero;
@@ -98,6 +131,7 @@ int main() {
     decimalParaOctal(numero);
     decimalParaHexadecimal(numero);
     decimalParaBCD(numero);
+    decimalParaComplemento2(numero);
 
     return 0;
 }
